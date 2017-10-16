9598a56426a7098d4aa16b74b50ba951b8e5b52b
“cchengying <374168328@qq.com>
2017-10-16 21:56:26 +0800

    d

---
+++ latte-ec/src/main/java/com/wei/latte/ec/sign/SignUpDelegate.java
@@ -0,0 +1,27 @@
+package com.wei.latte.ec.sign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.wei.latte.app.delegates.LatteDelegate;
+
+import butterknife.BindView;
+
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+
+public class SignUpDelegate extends LatteDelegate{
+
+
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle saveInstanceState, View rootView) {
+
+    }
+}
