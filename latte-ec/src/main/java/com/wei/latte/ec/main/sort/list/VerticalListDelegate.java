209ed586df03abf3854012c8f796cfce17d4f545
“cchengying <374168328@qq.com>
2017-11-06 19:44:07 +0800

    11-2

--- latte-ec/src/main/java/com/wei/latte/ec/main/sort/list/VerticalListDelegate.java
+++ latte-ec/src/main/java/com/wei/latte/ec/main/sort/list/VerticalListDelegate.java
@@ -2,10 +2,22 @@ package com.wei.latte.ec.main.sort.list;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.wei.latte.delegates.LatteDelegate;
 import com.wei.latte.ec.R;
+import com.wei.latte.ec.R2;
+import com.wei.latte.ec.main.sort.SortDelegate;
+import com.wei.latte.net.RestClient;
+import com.wei.latte.net.callback.ISuccess;
+import com.wei.latte.ui.recycler.MultipleItemEntity;
+
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * Created by Administrator on 2017/11/5.
@@ -13,14 +25,46 @@ import com.wei.latte.ec.R;
 
 public class VerticalListDelegate extends LatteDelegate{
 
+    @BindView(R2.id.rv_vertical_menu_list)
+    RecyclerView mRecyclerView = null;
 
     @Override
     public Object setLayout() {
         return R.layout.delegate_vertical_list;
     }
 
+    private void initRecyclerView() {
+        final LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(manager);
+        //屏蔽动画效果
+        mRecyclerView.setItemAnimator(null);
+
+    }
+
     @Override
     public void onBindView(@Nullable Bundle saveInstanceState, View rootView) {
+        initRecyclerView();
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+        RestClient.builder()
+                .url("sort_list_data")
+                .loader(getContext())
+                .success(new ISuccess() {
+                    @Override
+                    public void onSuccess(String response) {
+                        final List<MultipleItemEntity> data =
+                                new VerticalListDataConverter().setJsonData(response).convert();
+                        final SortDelegate delegate = getParentDelegate();
+                        //取父级delegate
+                        final SortRecyclerAdapter adapter = new SortRecyclerAdapter(data, delegate);
+                        mRecyclerView.setAdapter(adapter);
+                    }
+                })
+                .build()
+                .post();
 
     }
 }
